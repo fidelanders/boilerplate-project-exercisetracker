0cceb4019bd9e10df4f8a6d863da96a501c5d89b
Fidelis Ogbeni <85418769+fidelanders@users.noreply.github.com>
2023-02-07 14:54:47 +0100

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,15 +1,12 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-    _id:{ //PK
-      type: String
-    },
-    username:{
-      type: String,
-      required: true
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true
     }
-  })
+});
 
-const userModel = mongoose.model('User', userSchema)
-
-module.exports = userModel
+module.exports = mongoose.model('User', userSchema);
